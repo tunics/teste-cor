353f3aaba2447f78244a85a38fba12971e51ddac
Antonio <47387859+tunics@users.noreply.github.com>
2022-07-29 15:32:15 -0300

    Add files via upload

---
+++ js/inputs.js
@@ -0,0 +1,35 @@
+let requiredInputs = document.querySelectorAll("input:required");
+let requiredInputsArray = [...requiredInputs];
+
+// Mudas a cor da legenda para mostrar o erro em campos invalidos
+requiredInputsArray.forEach(function (input) {
+    input.addEventListener("focusout", function () {
+        let campoTxt = input.parentElement.parentElement;
+
+        if (input.checkValidity() == false) {
+            campoTxt.classList.add("erro");
+        } else {
+            campoTxt.classList.remove("erro");
+        }
+    });
+});
+
+// Verifica os campos desabilitados e deixa o botão dentro do campo desabilitado, caso exista
+const checkDisabledInputs = () => {
+    let inputs = document.querySelectorAll("input");
+    let inputsArray = [...inputs];
+
+    inputsArray.forEach(function (input) {
+        let campoBtn = input.parentElement.querySelector(".input-btn");
+
+        if (campoBtn != null && input.disabled == true) {
+            campoBtn.setAttribute("disabled", "");
+        } else if (campoBtn != null && input.disabled == false) {
+            campoBtn.removeAttribute("disabled", "");
+        } else {
+            console.log(input);
+        }
+    });
+};
+
+checkDisabledInputs();
